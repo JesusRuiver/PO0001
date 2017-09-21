f3fb06bc0300b7fbce5e3f097b8bf710c9f4cc62
JesusRuiver <jesusruiver@jesusruiver.com>
2017-09-21 18:44:31 +0200

    Falta el tratamiento de excepciones

--- PO0001/PO0001/src/ejercicio05/Notas.java
+++ PO0001/PO0001/src/ejercicio05/Notas.java
@@ -3,6 +3,9 @@
  */
 package ejercicio05;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Hashtable;
 
 /**
@@ -13,37 +16,37 @@ public class Notas {
 
 	/**
 	 * @param args
+	 * @throws IOException
+	 * @throws NumberFormatException
 	 */
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
 		/**
 		 * Utilizar un Hashtable con una capacidad de 10 y un factor de carga
 		 * del 80%. Contendrá los pares clave/valor de la siguiente tabla (las
 		 * claves serán objetos Integer, los valores Strings)
 		 */
-		
-		Hashtable<Integer, String> notas = new Hashtable<Integer,String>();
-		
+
+		Hashtable<Integer, String> notas = new Hashtable<Integer, String>();
+
 		notas.put(1505, "aprobado");
 		notas.put(2800, "suspenso");
 		notas.put(1300, "suspenso");
 		notas.put(1234, "aprobado");
 		notas.put(2600, "aprobado");
-		
-		
-		
+
 		/**
 		 * Crear una clase con un método main para saber si un usuario ha
 		 * aprobado o suspendido una prueba. El funcionamiento será el
 		 * siguiente: el programa solicitará una clave de usuario
 		 */
-		
-		
 
-		/**
-		 * Si no existe la clave, el programa debe mostrar "Clave incorrecta" y
-		 * finalizar.
-		 */
+		InputStreamReader entrada = new InputStreamReader(System.in);
+		BufferedReader leerEntrada = new BufferedReader(entrada);
+
+		System.out.print("Introduzca la clave de usuario: ");
+
+		Integer clave = new Integer(leerEntrada.readLine());
 
 		/**
 		 * Si existe, se mostrará su calificación y un mensaje de “Enhorabuena”
@@ -51,7 +54,23 @@ public class Notas {
 		 * finalizará.
 		 */
 
-		
+		if (clave.equals(1505) || clave.equals(1234) || clave.equals(2600)) {
+
+			System.out.println("El usuario " + clave + " esta "+ notas.getOrDefault(clave, "aprobado.") + " Enhorabuena");
+		} else if (clave.equals(2800) || clave.equals(1300)) {
+			System.out.println("El usuario " + clave + " esta " + notas.getOrDefault(clave, " suspenso.") + "Lo siento");
+		}
+		/**
+		 * Si no existe la clave, el programa debe mostrar "Clave incorrecta" y
+		 * finalizar.
+		 */
+
+		else {
+
+			System.out.println("La clave " + clave + " es incorrecta");
+			System.exit(0);
+		}
+
 	}
 
 }
