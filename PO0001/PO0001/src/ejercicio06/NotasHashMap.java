db408474c2e3e9549c5360aebaed6f6e310d9f3a
JesusRuiver <jesusruiver@jesusruiver.com>
2017-09-23 18:05:14 +0200

    Preparando ejercicio06

---
+++ PO0001/PO0001/src/ejercicio06/NotasHashMap.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package ejercicio06;
+
+import java.util.HashMap;
+
+/**
+ * @author Jesús Manuel Ruiz Verdejo
+ *
+ */
+public class NotasHashMap {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		/**
+		 * Utilizar un Hashtable con una capacidad de 10 y un factor de carga
+		 * del 80%. Contendrá los pares clave/valor de la siguiente tabla (las
+		 * claves serán objetos Integer, los valores Strings)
+		 */
+		
+		
+		/**
+		 * Crear una clase con un método main para saber si un usuario ha
+		 * aprobado o suspendido una prueba. El funcionamiento será el
+		 * siguiente: el programa solicitará una clave de usuario
+		 */
+		
+		
+		
+		/**
+		 * Si existe, se mostrará su calificación y un mensaje de “Enhorabuena”
+		 * si ha aprobado o un "Lo siento" si ha suspendido. Después el programa
+		 * finalizará.
+		 */
+		
+		/**
+		 * Si no existe la clave, el programa debe mostrar "Clave
+		 * incorrecta" y finalizar.
+		 */
+		
+		
+	}
+
+}
