429dc8f1eaa351dc5c5bff0b16f1b72c64178b85
JesusRuiver <jesusruiver@jesusruiver.com>
2017-09-24 23:32:39 +0200

    Terminado Algoritmo ejercicio 07

--- PO0001/PO0001/src/ejercicio07/ConversorMoneda.java
+++ PO0001/PO0001/src/ejercicio07/ConversorMoneda.java
@@ -7,7 +7,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author Jesús Manuel Ruiz Verdejo
@@ -28,30 +31,37 @@ public class ConversorMoneda {
 		monedas.put("libra", new BigDecimal(0.8852));
 		monedas.put("yen", new BigDecimal(133.9225));
 
-		System.out.print("Introduzca moneda de origen: ");
-		BufferedReader entradaOrigen = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Bienvenido al Conversor de Divisas Internacional");
+		System.out.println("Puede elegir entre las siguientes divisas: ");
 
-		String monedaOrigen = entradaOrigen.readLine().toLowerCase();
+		
 
-		System.out.print("Introduzca moneda de destino: ");
-		BufferedReader entradaDestino = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Introduzca moneda de origen: ");
+			BufferedReader entradaOrigen = new BufferedReader(new InputStreamReader(System.in));
 
-		String monedaDestino = entradaDestino.readLine().toLowerCase();
+			String monedaOrigen = entradaOrigen.readLine().toLowerCase();
 
-		System.out.print("Introduzca importe a convertir: ");
-		BufferedReader entradaImporte = new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Introduzca moneda de destino: ");
+			BufferedReader entradaDestino = new BufferedReader(new InputStreamReader(System.in));
 
-		BigDecimal importe = new BigDecimal(entradaImporte.readLine());
+			String monedaDestino = entradaDestino.readLine().toLowerCase();
 
-		if (monedas.containsKey(monedaOrigen) && monedas.containsKey(monedaDestino)) {
+			System.out.print("Introduzca importe a convertir: ");
+			BufferedReader entradaImporte = new BufferedReader(new InputStreamReader(System.in));
 
-			BigDecimal resultado = monedas.get(monedaDestino).divide(monedas.get(monedaOrigen));
-			
-			
-			System.out.println(resultado);
+			BigDecimal importe = new BigDecimal(entradaImporte.readLine());
 
-		}else{
-			System.out.println("Una de las claves no existe");
+			if (monedas.containsKey(monedaOrigen) && monedas.containsKey(monedaDestino)) {
+
+				BigDecimal resultado = monedas.get(monedaDestino)
+						.divide(monedas.get(monedaOrigen), RoundingMode.HALF_UP).multiply(importe);
+
+				System.out.println(
+						"El importe de: " + importe + " " + monedaOrigen + " son " + resultado + " " + monedaDestino);
+
+			} else {
+				System.out.println("Una de las claves no existe");
+			}
 		}
 	}
-}
+
