17d79d4c3506aaeb09ee5e6e6f27644a7332dc16
JesusRuiver <jesusruiver@jesusruiver.com>
2017-09-29 19:53:24 +0200

    Abrindo archivo contador

--- PO0001/PO0001/src/ejercicio09/CuentaPalabras.java
+++ PO0001/PO0001/src/ejercicio09/CuentaPalabras.java
@@ -14,7 +14,6 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-
 /**
  * @author Jesús Manuel Ruiz Verdejo
  *
@@ -25,67 +24,79 @@ public class CuentaPalabras {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		String origen ="texto.txt"; //Ruta relativa hace referencia al .project
-		String destino ="contador.txt";
-		
+
+		String origen = "texto.txt"; // Ruta relativa hace referencia donde se
+										// encuentra .project
+		String destino = "contador.txt";
+
 		HashMap<String, Integer> hm = new HashMap<String, Integer>();
-		
-		try{
-			//Lectura del fichero, contamos las plabra cuardandolas en un HashMap
-			
+
+		try {
+			// Lectura del fichero, contamos las plabra cuardandolas en un
+			// HashMap
+
 			FileReader fr = new FileReader(origen);
 			BufferedReader br = new BufferedReader(fr);
-			
+
 			String siguienteLinea;
-			
-			while((siguienteLinea = br.readLine()) != null){
+
+			while ((siguienteLinea = br.readLine()) != null) {
 				StringTokenizer st = new StringTokenizer(siguienteLinea, " ");
-				
-				while (st.hasMoreTokens()){
+
+				while (st.hasMoreTokens()) {
 					String palabra = st.nextToken();
-					
-					if(hm.containsKey(palabra)){
-						
+
+					if (hm.containsKey(palabra)) {
+
 						int contador = hm.get(palabra);
 						hm.remove(palabra);
-						hm.put(palabra, contador+1);
-					} else hm.put(palabra, 1);
-					
+						hm.put(palabra, contador + 1);
+					} else
+						hm.put(palabra, 1);
+
 				}
 			}
 			br.close();
-			
-			
-		}catch (IOException e){
-			
-			System.out.println("ERROR---"+ e.getMessage());
-				}
-		
+
+		} catch (IOException e) {
+
+			System.out.println("ERROR---" + e.toString());
+		}
+
 		// Pasamos las palabras del HashMap a un arrayList
-		
+
 		Set<String> claves1 = hm.keySet();
 		ArrayList<String> lista = new ArrayList<String>(claves1);
-		
-		//Ordenamos
-		 
-		 Collections.sort(lista); //argumento List, interfaz implementada
-		 
-		 //Creamos el fichero de salida
-		 
-		 try{
-			 
-			 FileWriter fw = new FileWriter(destino);
-			 
-			 BufferedWriter brw = new BufferedWriter(fw);
-			 
-			 
-			 
-		 } catch (Exception e) {
+
+		// Ordenamos
+
+		Collections.sort(lista); // argumento List, interfaz implementada
+
+		// Creamos el fichero de salida
+
+		try {
+
+			FileWriter fw = new FileWriter(destino);
+
+			BufferedWriter brw = new BufferedWriter(fw);
+
+			for (String clave : lista) {
+				brw.write(clave + " " + hm.get(clave));
+				brw.newLine();
+				
+			FileReader fr = new FileReader(destino);
+			BufferedReader br = new BufferedReader(fr);
 			
-		}
+			String texto = br.readLine();
+			
+			System.out.println(texto);
 			}
-	
-		
+			brw.close();
+
+		} catch (Exception e) {
+			System.out.println("ERROR---" + e.toString());
+
+		}
+	}
 
 }
